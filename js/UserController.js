5abfc3af28fd13a35bdc2ce78f2e6ba5d2f2ad2e
puppe1990 <matheus.puppe90@hotmail.com>
2018-10-07 20:52:49 -0300

    Requesting github user data

--- js/UserController.js
+++ js/UserController.js
@@ -26,12 +26,31 @@ class UserController {
         //send request to api
         xhr.send();
         //treats data asynchronously
-        xhr.onload = function(e) {
+        
+        xhr.onload = function() {
             //saves api response in a variable 
             //and transforms response into a valid JSON
-            const res = JSON.parse(xhr.response);
-            console.log(res);
+            if (xhr.status === 200) {
+                const res = JSON.parse(xhr.response);
+                console.log(res);
+                let div = document.getElementById("result");
+                createTd('td', res.avatar_url, div);
+                createTd('td', res.bio, div);
+                createTd('td', res.email, div);
+                createTd('td', res.followers, div);
+                createTd('td', res.following, div);
+
+                //res.bio,res.email,res.followers,res.following
+                
+            }else{
+                alert("Usuário Inválido!");
+            }   
+        }
+        function createTd(tag, data, div){
+            let element = document.createElement(tag);
+            let value = document.createTextNode(data);
+            element.appendChild(value);
+            div.appendChild(element);
         }
     }
-
 }
